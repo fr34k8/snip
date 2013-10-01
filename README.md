0c1b2cec71c0a9db65763fe27a34f9851d7e8e1b
Richard Spindler <richard@lateralblast.com.au>
2013-10-01 10:12:03 +1000

    first commit

---
+++ README.md
@@ -0,0 +1,22 @@
+snip
+====
+
+Service Now Information Processor
+
+Processes a Service Now XLSX CMDB extract
+
+Usage
+=====
+
+	$ snip -h
+	
+	-h: Display help/usage
+	-V: Display version
+	-c: Check CMDB data
+
+Examples
+========
+
+Check CMDB extract
+
+	$ snip -c
---
+++ snip.pl
@@ -0,0 +1,164 @@
+#!/usr/bin/env perl
+
+use strict;
+use Spreadsheet::XLSX;
+use Getopt::Std;
+use Text::Iconv;
+
+# Name:         snip.pl 
+# Version:      0.0.1
+# Release:      1
+# License:      Open Source 
+# Group:        System
+# Source:       Lateral Blast 
+# URL:          N/A
+# Distribution: CMDB 
+# Vendor:       UNIX
+# Packager:     Richard Spindler <richard@lateralblast.com.au>
+# Description:  Example script to process a Service Now CMDB extract
+
+# Changes       0.0.1 Tue  1 Oct 2013 09:15:04 EST
+#               Initial version
+
+my $script_name=$0;
+my $script_version=`cat $script_name | grep '^# Version' |awk '{print \$3}'`;
+my $options="chV";
+my %option;
+my @cmdb_data;
+my $cmdb_file="cmdb.xlsx";
+
+if ($#ARGV == -1) {
+  print_usage();
+}
+else {
+  getopts($options,\%option);
+}
+
+# If given -h print usage
+
+if ($option{'h'}) {
+  print_usage();
+  exit;
+}
+
+# Print script version
+
+if ($option{'V'}) {
+  print_version();
+  exit;
+}
+
+# If given -c check CMDB data
+
+if ($option{'c'}) {
+  import_cmdb_data();
+  check_cmdb_data();
+  exit;
+}
+
+# Print usage
+
+sub print_usage {
+  print "\n";
+  print "Usage: $script_name -$options\n";
+  print "\n";
+  print "-h: Display help/usage\n";
+  print "-V: Display version\n";
+  print "-c: Check CMDB data\n";
+  print "\n";
+  return;
+}
+
+# Print version
+
+sub print_version {
+  print "$script_version";
+  return;
+}
+
+# Import CMDB
+# Get the information we need and put it into an array
+
+sub import_cmdb_data {
+  my $host_name;
+  my @data;
+  my $line;
+  my $parser=Text::Iconv->new("utf-8", "windows-1251");
+  my $excel=Spreadsheet::XLSX ->new($cmdb_file,$parser);
+  foreach my $sheet (@{$excel->{Worksheet}}) {
+    $sheet->{MaxRow}||=$sheet->{MinRow};
+    foreach my $row ($sheet->{MinRow}..$sheet->{MaxRow}) {
+      $sheet->{MaxCol}||=$sheet->{MinCol};
+      @data=();
+      $line="";
+      foreach my $col ($sheet->{MinCol}..$sheet->{MaxCol}) {
+        my $cell=$sheet->{Cells}[$row][$col];
+        $cell=$cell->{Val};
+        $cell=~s/\n/ /g;
+        push(@data,$cell);
+      }
+      $line=join(",",@data);
+      if ($line!~/OS Service Pack/) {
+        push(@cmdb_data,$line);
+      }
+    }
+    return;
+  }
+}
+
+# Check CMDB data
+# Name,Class,Short description,Manufacturer,Location,OS Service Pack,OS Version,OS Address Width (bits),OS Domain,Operating System,Operational status
+
+sub check_cmdb_data {
+  my $line;
+  my $lc_line;
+  my $junk;
+  my @data;
+  my $host_name;
+  my $host_class;
+  my $host_info;
+  my $vendor_info;
+  my $loc_info;
+  my $os_rev;
+  my $os_ver;
+  my $os_width;
+  my $os_domain;
+  my $os_name;
+  my $status;
+  my $lc_status;
+  foreach $line (@cmdb_data) {
+    $lc_line=lc($line);
+    @data=split(/,/,$line);
+    $host_name=@data[0];
+    $host_class=@data[1];
+    $host_info=@data[2];
+    $vendor_info=@data[3];
+    $loc_info=@data[4];
+    $os_rev=@data[5];
+    $os_ver=@data[6];
+    $os_width=@data[7];
+    $os_domain=@data[8];
+    $os_name=@data[9];
+    $status=@data[10];
+    $lc_status=lc($status);
+    if ($host_name=~/\s+-/) {
+      ($host_name,$junk)=split(/\s+-/,$host_name);
+      print "$host_name contains a description in the Hostname field\n";
+    }
+    if ($lc_line!~/dev|prod|test/) {
+      print "$host_name does not contain any environment information (e.g. Dev / Prod / Test)\n";
+    }
+    if ($os_name!~/[A-z]/) {
+      print "$host_name does not contain any OS information (e.g. Dev / Prod / Test)\n";
+    }
+    if ($os_ver!~/[0-9]/) {
+      print "$host_name does not contain any OS version information (e.g. Dev / Prod / Test)\n";
+    }
+    if ($os_rev!~/[0-9]/) {
+      print "$host_name does not contain any OS revision information (e.g. Dev / Prod / Test)\n";
+    }
+    if ($lc_status!~/operational|decom/) {
+      print "$host_name does not contain any operational status information\n";
+    }
+  }
+}
