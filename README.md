9de05533d8689827f3f21aca5f03994b3429adbc
Richard Spindler <richard@lateralblast.com.au>
2014-06-17 10:55:50 +1000

    Documentation and license update

--- README.md
+++ README.md
@@ -1,23 +1,48 @@
-snip
+![alt tag](https://raw.githubusercontent.com/lateralblast/snip/master/snip.jpg)
+
+SNIP
 ====
 
 Service Now Information Processor
 
+Information
+-----------
+
 Processes a Service Now XLSX CMDB extract
 
-Usage
-=====
+License
+-------
 
-	$ snip -h
-	
-	-h: Display help/usage
-	-V: Display version
-	-c: Check CMDB data
-	-i: Input file (Default ./cmdb.xlsx)
+This software is licensed as CC-BA (Creative Commons By Attrbution)
+
+http://creativecommons.org/licenses/by/4.0/legalcode
+
+Usage
+-----
+
+```
+$ snip.pl -h
+
+-h: Display help/usage
+-V: Display version
+-c: Check CMDB data
+-i: Input file (Default ./cmdb.xlsx)
+```
 
 Examples
-========
+--------
 
 Check CMDB extract
 
-	$ snip -c -i CMDB.xlsx
+```
+$ snip.pl -c -i CMDB.xlsx
+```
+
+Requirements
+------------
+
+Perl Modules:
+
+- use Spreadsheet::XLSX
+- Getopt::Std
+- Text::Iconv
--- snip.pl
+++ snip.pl
@@ -1,35 +1,29 @@
 #!/usr/bin/env perl
 
+# Name:         snip.pl
+# Version:      0.0.4
+# Release:      1
+# License:      CC-BA (Creative Commons By Attrbution)
+#               http://creativecommons.org/licenses/by/4.0/legalcode
+# Group:        System
+# Source:       Lateral Blast
+# URL:          N/A
+# Distribution: CMDB
+# Vendor:       UNIX
+# Packager:     Richard Spindler <richard@lateralblast.com.au>
+# Description:  Example Perl script to process a Service Now CMDB extract
+
 use strict;
 use Spreadsheet::XLSX;
 use Getopt::Std;
 use Text::Iconv;
 
-# Name:         snip.pl 
-# Version:      0.0.3
-# Release:      1
-# License:      Open Source 
-# Group:        System
-# Source:       Lateral Blast 
-# URL:          N/A
-# Distribution: CMDB 
-# Vendor:       UNIX
-# Packager:     Richard Spindler <richard@lateralblast.com.au>
-# Description:  Example script to process a Service Now CMDB extract
-
-# Changes       0.0.1 Tue  1 Oct 2013 09:15:04 EST
-#               Initial version
-#               0.0.2 Tue  1 Oct 2013 11:31:26 EST
-#               Cleaned up output messages
-#               0.0.3 Tue  1 Oct 2013 11:37:08 EST
-#               Added -i switch to choose input file
-
-my $script_name=$0;
-my $script_version=`cat $script_name | grep '^# Version' |awk '{print \$3}'`;
-my $options="chVi:";
+my $script_name    = $0;
+my $script_version = `cat $script_name | grep '^# Version' |awk '{print \$3}'`;
+my $options        = "chVi:";
 my %option;
 my @cmdb_data;
-my $cmdb_file="cmdb.xlsx";
+my $cmdb_file      = "cmdb.xlsx";
 
 if ($#ARGV == -1) {
   print_usage();
@@ -41,7 +35,7 @@ else {
 # If given -i set input file to file given
 
 if ($option{'i'}) {
-  $cmdb_file=$option{'i'};
+  $cmdb_file = $option{'i'};
 }
 
 # If given -h print usage
@@ -106,22 +100,22 @@ sub import_cmdb_data {
   my $host_name;
   my @data;
   my $line;
-  my $parser=Text::Iconv->new("utf-8", "windows-1251");
-  my $excel=Spreadsheet::XLSX ->new($cmdb_file,$parser);
+  my $parser = Text::Iconv->new("utf-8", "windows-1251");
+  my $excel  = Spreadsheet::XLSX ->new($cmdb_file,$parser);
   foreach my $sheet (@{$excel->{Worksheet}}) {
     $sheet->{MaxRow}||=$sheet->{MinRow};
     foreach my $row ($sheet->{MinRow}..$sheet->{MaxRow}) {
       $sheet->{MaxCol}||=$sheet->{MinCol};
-      @data=();
-      $line="";
+      @data = ();
+      $line = "";
       foreach my $col ($sheet->{MinCol}..$sheet->{MaxCol}) {
-        my $cell=$sheet->{Cells}[$row][$col];
-        $cell=$cell->{Val};
-        $cell=~s/\n/ /g;
+        my $cell = $sheet->{Cells}[$row][$col];
+        $cell = $cell->{Val};
+        $cell =~ s/\n/ /g;
         push(@data,$cell);
       }
-      $line=join(",",@data);
-      if ($line!~/OS Service Pack/) {
+      $line = join(",",@data);
+      if ($line !~ /OS Service Pack/) {
         push(@cmdb_data,$line);
       }
     }
@@ -150,37 +144,37 @@ sub check_cmdb_data {
   my $status;
   my $lc_status;
   foreach $line (@cmdb_data) {
-    $lc_line=lc($line);
-    @data=split(/,/,$line);
-    $host_name=@data[0];
-    $host_class=@data[1];
-    $host_info=@data[2];
-    $vendor_info=@data[3];
-    $loc_info=@data[4];
-    $os_rev=@data[5];
-    $os_ver=@data[6];
-    $os_width=@data[7];
-    $os_domain=@data[8];
-    $os_name=@data[9];
-    $status=@data[10];
-    $lc_status=lc($status);
-    if ($host_name=~/\s+-/) {
-      ($host_name,$junk)=split(/\s+-/,$host_name);
+    $lc_line     = lc($line);
+    @data        = split(/,/,$line);
+    $host_name   = @data[0];
+    $host_class  = @data[1];
+    $host_info   = @data[2];
+    $vendor_info = @data[3];
+    $loc_info    = @data[4];
+    $os_rev      = @data[5];
+    $os_ver      = @data[6];
+    $os_width    = @data[7];
+    $os_domain   = @data[8];
+    $os_name     = @data[9];
+    $status      = @data[10];
+    $lc_status   = lc($status);
+    if ($host_name =~ /\s+-/) {
+      ($host_name,$junk) = split(/\s+-/,$host_name);
       print "$host_name contains a description in the Hostname field\n";
     }
-    if ($lc_line!~/dev|prod|test/) {
+    if ($lc_line !~ /dev|prod|test/) {
       print "$host_name does not contain any environment information (e.g. Dev / Prod / Test)\n";
     }
-    if ($os_name!~/[A-z]/) {
+    if ($os_name !~ /[A-z]/) {
       print "$host_name does not contain any OS information\n";
     }
-    if ($os_ver!~/[0-9]/) {
+    if ($os_ver !~ /[0-9]/) {
       print "$host_name does not contain any OS version information\n";
     }
-    if ($os_rev!~/[0-9]/) {
+    if ($os_rev !~ /[0-9]/) {
       print "$host_name does not contain any OS revision information\n";
     }
-    if ($lc_status!~/operational|decom/) {
+    if ($lc_status !~ /operational|decom/) {
       print "$host_name does not contain any operational status information\n";
     }
   }
